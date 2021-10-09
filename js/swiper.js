0922c45c91c039163249d818a7c00faba4c5b018
santhosh <santhosh.battini@mtxb2b.com>
2021-10-09 18:25:24 +0530

    banner

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.slideNext-btn',
+        prevEl: '.slidePrev-btn',
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
